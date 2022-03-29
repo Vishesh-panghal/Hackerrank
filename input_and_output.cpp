2ecb89210b998d1a882b9a705371e0051f2b9d64
Vishesh Panghal <visheshpanghal12@gmail.com>
2022-03-29 12:55:46 +0530

    Intro

---
+++ input_and_output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int a,b,c;
+   cin>>a>>b>>c;
+   int ans = a + b+ c;
+   cout<<ans;
+    return 0;
+}
